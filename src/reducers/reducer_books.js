a784fc1eef6aafed425114fa7cae060506923d31
Jakub Wasiljew <websolutionslab@gmail.com>
2017-09-23 00:29:13 +0200

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default fun = () => {
+    return [
+        {title: 'You dont know Js'},
+        {title: 'Js the good parts'},
+        {title: 'Js the weird parts'},
+        {title: 'Citizen Kane'}
+    ]
+}
