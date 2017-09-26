5b1b00f206e89aed59d9fe3a5355ac41b06b2c58
Jakub Wasiljew <websolutionslab@gmail.com>
2017-09-27 00:51:19 +0200

    first action added

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
     renderList = () => {
         return this.props.books.map(( book ) => {
             return (
-                <li key={book.title} className="list-group-item">
+                <li
+                    onClick={() => this.props.selectBook(book)}
+                    key={book.title}
+                    className="list-group-item">
                     {book.title}
                 </li>
             );
@@ -28,4 +33,14 @@ function mapStateToProps( state ) {
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+// returned props on the book list container
+function mapDispatchProps( dispatch ) {
+    //when book is called result passed to all reducers
+    return bindActionCreators({
+        selectBook: selectBook
+    }, dispatch)
+}
+
+// Promote BookList feom component to a container - it needs to know about dispatch method.
+// Available as props
+export default connect(mapStateToProps, mapDispatchProps)(BookList);
